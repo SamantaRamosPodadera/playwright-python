0c7927262d93df843e4aba40bd75f99bf681be05
Samanta Ramos Podadera <161749959+SamantaRamosPodadera@users.noreply.github.com>
2024-10-18 18:45:28 +0200

    magic

--- tests/test_visit_page.py
+++ tests/test_visit_page.py
@@ -2,7 +2,6 @@ import re
 from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("https://bootcampqa.com/")
 
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
+
